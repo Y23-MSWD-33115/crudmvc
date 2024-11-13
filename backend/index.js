78bc2cf4ef8a1b0b6c88897805601e61e754604f
roshan <roshanananthasai@gmail.com>
2024-11-13 10:59:33 +0530

    Initial commit for crudmvc project

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const studentRoutes = require('./routes/studentRoutes');
+const path = require('path');
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api', studentRoutes);
+
+// Serve React App
+app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+});
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
